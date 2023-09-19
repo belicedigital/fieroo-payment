26810f77ee3308a0d7dd021f2f45464b9d4980a0
Francesco Passanante <francescopassanante@gmail.com>
2023-09-19 12:18:26 +0200

    set env PAYPAL_TEST_MODE

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -33,7 +33,7 @@ class PaymentController extends Controller
         $this->gateway = Omnipay::create('PayPal_Rest');
         $this->gateway->setClientId(env('PAYPAL_CLIENT_ID'));
         $this->gateway->setSecret(env('PAYPAL_CLIENT_SECRET'));
-        $this->gateway->setTestMode(true);
+        $this->gateway->setTestMode(env('PAYPAL_TEST_MODE'));
     }
 
     public function pay(Request $request)
