6c256debecaf8bedf3770082764ceee929a71bd4
Francesco Passanante <francescopassanante@gmail.com>
2023-09-20 18:49:38 +0200

    continue stripe implement

--- src/Controllers/StripePaymentController.php
+++ src/Controllers/StripePaymentController.php
@@ -49,6 +49,43 @@ class StripePaymentController extends Controller
             // if is not a customer is created
             $customer = $exhibitor->createOrGetStripeCustomer();
 
+            // updt customer data
+            $exhibitor_detail = $exhibitor->detail;
+            $data_for_billing = [
+                'address' => [
+                    'city' => $exhibitor_detail->city,
+                    'postal_code' => $exhibitor_detail->cap,
+                    'state' => $exhibitor_detail->province,
+                    'line1' => $exhibitor_detail->address.', '.$exhibitor_detail->civic_number,
+                ],
+                'email' => $exhibitor->user->email,
+                'name' => $exhibitor_detail->responsible,
+                'phone' => $exhibitor_detail->phone_responsible,
+                'preferred_locales' => [ $exhibitor->locale ],
+            ];
+            $vat_number = $exhibitor_detail->vat_number;
+            if($exhibitor_detail->diff_billing) {
+                $data_for_billing = [
+                    'address' => [
+                        'city' => $exhibitor_detail->receiver_city,
+                        'postal_code' => $exhibitor_detail->receiver_cap,
+                        'state' => $exhibitor_detail->receiver_province,
+                        'line1' => $exhibitor_detail->receiver_address.', '.$exhibitor_detail->receiver_civic_number,
+                    ],
+                    'email' => $exhibitor->user->email,
+                    'name' => $exhibitor_detail->responsible,
+                    'phone' => $exhibitor_detail->phone_responsible,
+                    'preferred_locales' => [ $exhibitor->locale ],
+                ];
+                $vat_number = $exhibitor_detail->receiver_vat_number;
+            }
+
+            $exhibitor->updateStripeCustomer($data_for_billing);
+
+            if($exhibitor->taxIds()->count() <= 0) {
+                $exhibitor->createTaxId('eu_vat', $vat_number);
+            }
+
             $stripeCharge = $request->user()->exhibitor->charge(
                 $amount, $request->paymentMethodId, [
                     'customer' => $customer->id,
@@ -63,15 +100,28 @@ class StripePaymentController extends Controller
                 ]
             );
 
-            $updt_exhibitor = Exhibitor::findOrFail($exhibitor->id);
-            
-            if(is_null($exhibitor->pm_type)) {
-                $updt_exhibitor->pm_type = $stripeCharge->charges->data[0]->payment_method_details->type;
-            }
-            if(is_null($exhibitor->pm_last_four)) {
-                $updt_exhibitor->pm_last_four = $stripeCharge->charges->data[0]->payment_method_details->card->last4;
-            }
+            // $request->user()->exhibitor->invoicePrice(10, 5);
+            // $request->user()->exhibitor->invoiceFor('prova', 1, [
+            //     'price_data' => [
+            //         'unit_amount'=> 1000,
+            //         'currency' => 'eur',
+            //         'product'=> [
+            //           'name'=> 'prod_Ofot7Gv2iaQ07b'
+            //         ],
+            //         'tax_behavior'=>'exclusive',
+        
+            //     ]
+            // ]);
+            // invoiceFor fa anche il charge però sembra che non venga completato,  però almeno genera la fattura ma mancano le info  i tassi ed altro.
+            $request->user()->exhibitor->invoiceFor('Stickers', 500, [
+                'quantity' => 50,
+            ], [
+                'default_tax_rates' => ['tax-rate-id'],
+            ]);
 
+            $updt_exhibitor = Exhibitor::findOrFail($exhibitor->id);
+            $updt_exhibitor->pm_type = $stripeCharge->charges->data[0]->payment_method_details->type;
+            $updt_exhibitor->pm_last_four = $stripeCharge->charges->data[0]->payment_method_details->card->last4;
             $updt_exhibitor->save();
 
         } catch(\Throwable $th){
