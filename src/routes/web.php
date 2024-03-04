1025edaa30c2ddcb16c289279ba5977479cfe501
Francesco Passanante <francescopassanante@gmail.com>
2024-03-04 21:59:36 +0100

    fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,14 +4,14 @@ use Fieroo\Payment\Controllers\PaymentController;
 use Fieroo\Payment\Controllers\StripePaymentController;
 
 Route::group(['prefix' => 'admin', 'middleware' => ['web','auth']], function() {
-    Route::group(['prefix' => 'paypal'], function() {
-        Route::post('/', [PaymentController::class, 'pay'])->name('payment');
-        Route::post('/furnishings', [PaymentController::class, 'payFurnishings'])->name('payment-furnishings');
-        Route::get('/success', [PaymentController::class, 'success']);
-        Route::get('/success-furnishings', [PaymentController::class, 'successFurnishings']);
-        Route::get('/error', [PaymentController::class, 'error']);
-    });
+    // Route::group(['prefix' => 'paypal'], function() {
+    //     Route::post('/', [PaymentController::class, 'pay'])->name('payment');
+    //     Route::post('/furnishings', [PaymentController::class, 'payFurnishings'])->name('payment-furnishings');
+    //     Route::get('/success', [PaymentController::class, 'success']);
+    //     Route::get('/success-furnishings', [PaymentController::class, 'successFurnishings']);
+    //     Route::get('/error', [PaymentController::class, 'error']);
+    // });
 
-    // Route::post('stripe-payment', [StripePaymentController::class, 'payment'])->name('stripe-payment');
-    // Route::post('/furnishings', [StripePaymentController::class, 'payFurnishings'])->name('payment-furnishings');
+    Route::post('stripe-payment', [StripePaymentController::class, 'payment'])->name('stripe-payment');
+    Route::post('/furnishings', [StripePaymentController::class, 'payFurnishings'])->name('payment-furnishings');
 });
