90a67aac437f1c31ec6b9139e2df854e0f07d5d8
Francesco Passanante <francescopassanante@gmail.com>
2024-03-04 17:25:31 +0100

    fix

--- src/Controllers/StripePaymentController.php
+++ src/Controllers/StripePaymentController.php
@@ -136,7 +136,7 @@ class StripePaymentController extends Controller
                 ->with('success', trans('generals.payment_subscription_ok', ['event' => $event->title]));
 
         } catch(IncompletePayment $exception) {
-            return redirect()->route('cashier.payment', [$exception->payment->id, 'redirect' => '3dsecure.auth']);
+            return redirect()->route('cashier.payment', [$exception->payment->id, 'redirect' => route('3dsecure.auth')]);
         } catch(\Throwable $th){
             return redirect()
                 ->back()
