a857d4b05613001a22a19479ba0a2646790f6fb9
Francesco Passanante <francescopassanante@gmail.com>
2024-03-04 22:19:23 +0100

    fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,7 +6,7 @@ use Fieroo\Payment\Controllers\StripePaymentController;
 Route::group(['prefix' => 'admin', 'middleware' => ['web','auth']], function() {
     Route::group(['prefix' => 'paypal'], function() {
         Route::post('/', [PaymentController::class, 'pay'])->name('payment');
-        Route::post('/furnishings', [PaymentController::class, 'payFurnishings'])->name('payment-furnishings');
+        Route::post('/furnishings-payment', [PaymentController::class, 'payFurnishings'])->name('furnishings-payment');
         Route::get('/success', [PaymentController::class, 'success']);
         Route::get('/success-furnishings', [PaymentController::class, 'successFurnishings']);
         Route::get('/error', [PaymentController::class, 'error']);
