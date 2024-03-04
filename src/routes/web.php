3d05f1586cfce5c2908c5f887976d176fa8fd9c0
Francesco Passanante <francescopassanante@gmail.com>
2024-03-04 22:12:43 +0100

    fix: comment route stripe

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,12 +6,12 @@ use Fieroo\Payment\Controllers\StripePaymentController;
 Route::group(['prefix' => 'admin', 'middleware' => ['web','auth']], function() {
     Route::group(['prefix' => 'paypal'], function() {
         Route::post('/', [PaymentController::class, 'pay'])->name('payment');
-        Route::post('/sfurnishings', [PaymentController::class, 'payFurnishings'])->name('spayment-furnishings');
+        Route::post('/furnishings', [PaymentController::class, 'payFurnishings'])->name('payment-furnishings');
         Route::get('/success', [PaymentController::class, 'success']);
         Route::get('/success-furnishings', [PaymentController::class, 'successFurnishings']);
         Route::get('/error', [PaymentController::class, 'error']);
     });
 
-    Route::post('stripe-payment', [StripePaymentController::class, 'payment'])->name('stripe-payment');
-    Route::post('/furnishings', [StripePaymentController::class, 'payFurnishings'])->name('payment-furnishings');
+    // Route::post('stripe-payment', [StripePaymentController::class, 'payment'])->name('stripe-payment');
+    // Route::post('/furnishings', [StripePaymentController::class, 'payFurnishings'])->name('payment-furnishings');
 });
